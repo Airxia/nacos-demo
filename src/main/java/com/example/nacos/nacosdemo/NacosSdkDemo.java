db7aff5249570964f6290df44c18304deafc2f25
alvin <491783100@qq.com>
2019-08-04 13:21:02 +0800

    基于SDK来访问改变Nacos配置中心信息

---
+++ src/main/java/com/example/nacos/nacosdemo/NacosSdkDemo.java
@@ -0,0 +1,34 @@
+package com.example.nacos.nacosdemo;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import javax.security.auth.login.Configuration;
+import java.util.Properties;
+
+/**
+ * @from: https://www.cnblogs.com/java333/
+ * @desc: TODO
+ * @author: jar luo
+ * @date: 2019/8/4 13:11
+ */
+public class NacosSdkDemo {
+    public static void main(String[] args) {
+        //连接到目标服务器地址
+        //指定dataId和groupId
+        String serverAddr = "localhost:8848";
+        String dataId = "example";
+        String groupId = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put("serverAddr",serverAddr);
+
+        try {
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            String  content = configService.getConfig(dataId,groupId,3000);
+            System.out.println(content);
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+    }
+}
